88408c0a0d0a6ba65a86e02bef2fd88c17ee5f9b
PrinceSingh786 <psjeee8@gmail.com>
2024-09-26 21:22:58 +0530

    First commit

---
+++ barks.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello guys")
+}
+
+func Bark() {
+	fmt.Println("Wooh! I am dog")
+}
+func Barks() {
+	fmt.Println("Wooh! I am dog")
+	fmt.Println("Wooh! I am dog")
+	fmt.Println("Wooh! I am dog")
+	fmt.Println("Wooh! I am dog")
+}
